dc2871e3d2d39d416facb2677a98afaa303092a0
ceas <ceasermakpwe@gmail.com>
2026-07-14 12:47:35 +0300

    fix: enforce onboarding@resend.dev when using Resend API to send to/from Gmail to avoid 403 error

--- campus_marketplace/settings.py
+++ campus_marketplace/settings.py
@@ -209,10 +209,9 @@ EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
-DEFAULT_FROM_EMAIL = os.getenv(
-    "DEFAULT_FROM_EMAIL",
-    "onboarding@resend.dev"
-    if RESEND_API_KEY
-    else os.getenv("EMAIL_HOST_USER", "ceasermakpwe@gmail.com"),
-)
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "")
+if RESEND_API_KEY and (not DEFAULT_FROM_EMAIL or "gmail.com" in DEFAULT_FROM_EMAIL.lower() or "googlemail.com" in DEFAULT_FROM_EMAIL.lower()):
+    DEFAULT_FROM_EMAIL = "onboarding@resend.dev"
+elif not DEFAULT_FROM_EMAIL:
+    DEFAULT_FROM_EMAIL = os.getenv("EMAIL_HOST_USER", "ceasermakpwe@gmail.com")
 EMAIL_TIMEOUT = int(os.getenv("EMAIL_TIMEOUT", "10"))
